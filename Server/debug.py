27e2b8a156ef8e195ce00b81a350719c21125daf
toastin0 <64383025+toastin0@users.noreply.github.com>
2022-01-18 13:17:13 -0500

    also cleaned up a bit

--- Server/debug.py
+++ Server/debug.py
@@ -1,14 +1,11 @@
-
-
-
 class Debug:
-    
-    def __init__(self):
-        pass
     PREFIX = "[DEBUG]: "
 
-    
+    def __init__(self):
+        pass
+
     def Dlog(self, Message):
         print(self.PREFIX + Message)
+
 
 d = Debug()
