beb36eceac8c67507144b406a86d920e310b6a4e
ShottyMan <Jyssauco1@gmail.com>
2022-01-21 18:02:38 -0500

    Began setting up the escapes

--- server/debug.py
+++ server/debug.py
@@ -1,11 +1,26 @@
+import time
 class Debug:
-    PREFIX = "[DEBUG]: "
-
     def __init__(self):
-        pass
-
-    def Dlog(self, Message):
-        print(self.PREFIX + Message)
+        #This is so that all of the variables are set up for the use of the functions
+        self.first_line_count = -1
+        self.PREFIX = "[DEBUG]: "
+        self.NEWCONN_PREFIX = "[\x1b[38;5;48mNEW CONNECTION\x1b[0m]: "
+        self.NOTICE_PREFIX = "[\x1b[38;5;220mNOTICE\x1b[0m]: "
+        self.ERROR_PREFIX = "[\x1b[38;5;124mERROR\x1b[0m]: "
+        self.WARNING_PREFIX = "[\x1b[38;5;202mWARNING\x1b[0m]: "
+        self.ESCAPE_CODE = f"""\x1b[1A\x1b[{self.first_line_count}C"""
+    def dlog_debug(self, Message):
+        time.sleep(1)
+        print(self.ESCAPE_CODE + self.PREFIX + Message)
+        self.first_line_count = len(self.PREFIX + Message)
+    def dlog_debugnewconn(self, Message):
+        print(self.NEWCONN_PREFIX + Message)
+    def dlog_notice(self, Message):
+        print(self.NOTICE_PREFIX + Message)
+    def dlog_error(self, Message):
+        print(self.ERROR_PREFIX + Message)
+    def dlog_warning(self, Message):
+        print(self.WARNING_PREFIX + Message)
 
 
 d = Debug()
--- server/mainprocess.py
+++ server/mainprocess.py
@@ -6,8 +6,9 @@ import clienthandling
 import scheduler
 import re
 import time
+import os
+from debug import d
 
-# from debug import d
 
 SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 START_THREAD = threading.active_count()
@@ -38,25 +39,39 @@ def make_event(user_input, sch_obj: scheduler.Scheduler):
     sch_obj.new_event(day, time_tuple[0], time_tuple[1])
     return 0
 
+#Create a queue for the Local control thread for allowing talking within and outside the thread.
 local_control_queue = clienthandling.Queue()
 
+def start_terminal():
+    columns, lines = os.get_terminal_size()
+    print(f"""\x1b[2J\x1b[{lines};0H""")
+
+#This is the function we pass to the thread function to start a new thread.
 def local_control():
     s = scheduler.Scheduler()
+    start_terminal()
+    d.dlog_debug("Thread has been started.")
     while True:
         user_input = input(">: ")
         if user_input == "quit":
             break
         if user_input == "mkevent": # FIXME: this... doesn't work
-            print(make_event(user_input, s))
+            d.dlog_notice(make_event(user_input, s))
         elif user_input == "showevents":
-            print(s.event_dict)  # TODO: make this prettier
+            d.dlog_notice(s.event_dict)  # TODO: make this prettier
     # TODO: make debug work again
+    local_control_queue.enqueue(True)
 
 def main():
+    #Started thread to allow input while outputting
+    thread = threading.Thread(target=local_control)
+    thread.start()
     
     while True:
-        thread = threading.Thread(target=local_control)
-        thread.start()
-        if local_control_queue:
-            print("[NOTICE]: Quitting Program...")
+        #Reminder if you want to pass arguements you can do args=*Place arguements here*
+        time.sleep(2)
+        d.dlog_debug("Testing")
+        if local_control_queue.dequeue():
+            d.dlog_notice("Program closing...")
             break
+    thread.join()
